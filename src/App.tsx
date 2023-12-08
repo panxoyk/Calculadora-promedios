c67ae3180909b3bbcd36c20bb3baef90168b6964
panxoyk <francisco.cstllhrnndz@gmail.com>
2023-12-08 04:07:55 -0300

    Detalles de la ventana actualizados

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
 		<ThemeProvider storageKey='vite-ui-theme'>
 			<div className='m-auto max-w-screen-lg my-8 relative'>
 				<AppTittle />
-				<div className='border-b pb-4 pt-4'>
+				<div className='border-b pb-8 pt-4'>
 					<div className='flex flex-col md:flex-row lg:w-3/4 gap-4 md:gap-0 mx-auto items-stretch'>
 						<AddPromediosForm />
 						<div className='max-md:hidden'>
