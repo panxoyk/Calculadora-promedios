7995c5855c89e0652469e86d63963e89583e63d5
panxoyk <francisco.cstllhrnndz@gmail.com>
2023-12-09 02:06:19 -0300

    Estados para distintos tipos de notas (added)

--- src/store/promediosStore.ts
+++ src/store/promediosStore.ts
@@ -1,18 +1,20 @@
 import { create } from "zustand";
-import { Promedio } from '../types/types'
+import { Promedio, Tipo } from '../types/types'
 import { promediosItem, setPromediosItem } from "@/utility/utility";
 
 interface PromediosState {
     promedios: Promedio[]
-    resultado: number
     addPromedio: () => void
-    changePromedioNombre: (idPromedio: number, nuevoNombre: string) => void
-    changePromedioPorcentaje: (idPromedio: number, nuevoPorcentaje: number | string) => void
     deletePromedio: (id: number) => void
+    changePromedioNombre: (idPromedio: number, nombre: string) => void
+    changePromedioPorcentaje: (idPromedio: number, porcentaje: number | string) => void
+    changePromedioTipo: (idPromedio: number, tipo: Tipo) => void
     addNota: (id: number) => void
-    changeNotaNombre: (idPromedio: number, idNota: number, nombre: string) => void
-    changeNotaEvaluacion: (idPromedio: number, idNota: number, evaluacion: number) => void
     deleteNota: (idPromedio: number, idNota: number) => void
+    changeNotaNombre: (idPromedio: number, idNota: number, nombre: string) => void
+    changeNotaCalificacion: (idPromedio: number, idNota: number, evaluacion: number) => void
+    changeNotaPorcentaje: (idPromedio: number, idNota: number, porcentaje: number | string) => void
+    resultado: number
     calculateResultado: () => void
 }
 
@@ -25,7 +27,6 @@ export const usePromediosStore = create<PromediosState>((set, get) => ({
         porcentaje: '',
         notas: [{ id: 1, nombre: '', evaluacion: '' }],
     }],
-    resultado: 0,
     addPromedio: () => {
         const { promedios } = get()
         set({
@@ -36,10 +37,22 @@ export const usePromediosStore = create<PromediosState>((set, get) => ({
                     nombre: '',
                     porcentaje: '',
                     notas: [{
-                        id: 1, nombre: '', evaluacion: ''
-                    }]
+                        id: 1, nombre: '', porcentaje: '', calificacion: ''
+                    }],
+                    tipo: 'promedio'
                 }
             ]
+        })
+        setPromediosItem(get().promedios)
+    },
+    deletePromedio: (idPromedio) => {
+        const { promedios } = get()
+        const promediosUpdated = promedios.filter((promedio) => promedio.id !== idPromedio)
+        promediosUpdated.forEach((promedio, index) => {
+            promedio.id = index + 1
+        })
+        set({
+            promedios: promediosUpdated
         })
         setPromediosItem(get().promedios)
     },
@@ -71,14 +84,17 @@ export const usePromediosStore = create<PromediosState>((set, get) => ({
         })
         setPromediosItem(get().promedios)
     },
-    deletePromedio: (idPromedio) => {
+    changePromedioTipo: (idPromedio, nuevoTipo) => {
         const { promedios } = get()
-        const promediosUpdated = promedios.filter((promedio) => promedio.id !== idPromedio)
-        promediosUpdated.forEach((promedio, index) => {
-            promedio.id = index + 1
-        })
         set({
-            promedios: promediosUpdated
+            promedios: promedios.map((promedio) =>
+                promedio.id === idPromedio
+                    ? {
+                        ...promedio,
+                        tipo: nuevoTipo,
+                    }
+                    : promedio
+            ),
         })
         setPromediosItem(get().promedios)
     },
@@ -92,47 +108,13 @@ export const usePromediosStore = create<PromediosState>((set, get) => ({
                             ...promedio.notas, {
                                 id: promedio.notas.length + 1,
                                 nombre: '',
-                                evaluacion: '',
+                                porcentaje: '',
+                                calificacion: '',
                             }
                         ]
                     }
                     : promedio
             )
-        })
-        setPromediosItem(get().promedios)
-    },
-    changeNotaNombre: (idPromedio, idNota, nuevoNombre) => {
-        const { promedios } = get();
-        set({
-            promedios: promedios.map((promedio) =>
-                promedio.id === idPromedio
-                    ? {
-                        ...promedio,
-                        notas: promedio.notas.map((nota) => nota.id === idNota
-                            ? { ...nota, nombre: nuevoNombre }
-                            : nota
-                        ),
-                    }
-                    : promedio
-            ),
-        })
-        setPromediosItem(get().promedios)
-    },
-    changeNotaEvaluacion: (idPromedio, idNota, nuevaEvaluacion) => {
-        const { promedios } = get();
-        set({
-            promedios: promedios.map((promedio) =>
-                promedio.id === idPromedio
-                    ? {
-                        ...promedio,
-                        notas: promedio.notas.map((nota) =>
-                            nota.id === idNota
-                                ? { ...nota, evaluacion: isNaN(nuevaEvaluacion) ? '' : nuevaEvaluacion }
-                                : nota
-                        )
-                    }
-                    : promedio
-            ),
         })
         setPromediosItem(get().promedios)
     },
@@ -154,25 +136,106 @@ export const usePromediosStore = create<PromediosState>((set, get) => ({
         })
         setPromediosItem(get().promedios)
     },
+    changeNotaNombre: (idPromedio, idNota, nuevoNombre) => {
+        const { promedios } = get();
+        set({
+            promedios: promedios.map((promedio) =>
+                promedio.id === idPromedio
+                    ? {
+                        ...promedio,
+                        notas: promedio.notas.map((nota) => nota.id === idNota
+                            ? { ...nota, nombre: nuevoNombre }
+                            : nota
+                        ),
+                    }
+                    : promedio
+            ),
+        })
+        setPromediosItem(get().promedios)
+    },
+    changeNotaPorcentaje: (idPromedio, idNota, nuevoPorcentaje) => {
+        const { promedios } = get();
+        set({
+            promedios: promedios.map((promedio) =>
+                promedio.id === idPromedio
+                    ? {
+                        ...promedio,
+                        notas: promedio.notas.map((nota) => nota.id === idNota
+                            ? { ...nota, porcentaje: nuevoPorcentaje }
+                            : nota
+                        ),
+                    }
+                    : promedio
+            ),
+        })
+        setPromediosItem(get().promedios)
+    },
+    changeNotaCalificacion: (idPromedio, idNota, nuevaCalificacion) => {
+        const { promedios } = get();
+        set({
+            promedios: promedios.map((promedio) =>
+                promedio.id === idPromedio
+                    ? {
+                        ...promedio,
+                        notas: promedio.notas.map((nota) =>
+                            nota.id === idNota
+                                ? { ...nota, calificacion: isNaN(nuevaCalificacion) ? '' : nuevaCalificacion }
+                                : nota
+                        )
+                    }
+                    : promedio
+            ),
+        })
+        setPromediosItem(get().promedios)
+    },
+    resultado: 0,
     calculateResultado: () => {
         const { promedios } = get()
-        let promedioFinal = 0
+        let notaFinal = 0
         promedios.map((promedio) => {
-            let promedioNotas = 0
-            promedio.notas.map((nota) => {
-                typeof (nota.evaluacion) === 'number'
-                    ? promedioNotas += nota.evaluacion
-                    : promedioNotas
-            })
-            promedio.notas.length === 0
-                ? promedioNotas = 0
-                : promedioNotas = promedioNotas / promedio.notas.length
-            typeof (promedio.porcentaje) === 'number'
-                ? promedioFinal += promedioNotas * promedio.porcentaje / 100
-                : promedioFinal
+            switch(promedio.tipo) {
+                case 'promedio': {
+                    let promedioNotas = 0
+                    promedio.notas.map((nota) => {
+                        typeof (nota.calificacion) === 'number'
+                            ? promedioNotas += nota.calificacion
+                            : promedioNotas
+                    })
+                    promedio.notas.length === 0
+                        ? promedioNotas = 0
+                        : promedioNotas = promedioNotas / promedio.notas.length
+                    typeof (promedio.porcentaje) === 'number'
+                        ? notaFinal += promedioNotas * promedio.porcentaje / 100
+                        : notaFinal
+                    break
+                }
+                case 'nota': {
+                    const nota = promedio.notas[0].calificacion
+                    const porcentaje = promedio.porcentaje
+                    typeof (nota) === 'number' && typeof(porcentaje) === 'number'
+                        ? notaFinal += nota * porcentaje / 100
+                        : notaFinal
+                    break
+                }
+                case 'personalizado': {
+                    let promedioNotas = 0
+                    promedio.notas.map((nota) => {
+                        typeof (nota.calificacion) === 'number' && typeof (nota.porcentaje) === 'number'
+                            ? promedioNotas += nota.calificacion * nota.porcentaje / 100
+                            : promedioNotas
+                    })
+                    typeof (promedio.porcentaje) === 'number'
+                        ? notaFinal += promedioNotas * promedio.porcentaje / 100
+                        : notaFinal
+                    break
+                }
+                default: {
+                    break
+                }
+            }
         })
         set({
-            resultado: Number(promedioFinal.toFixed(2))
+            resultado: Number(notaFinal.toFixed(2))
         })
     },
 }))
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,12 +1,16 @@
+export type Tipo = 'promedio' | 'nota' | 'personalizado'
+
 export interface Promedio {
     id: number
     nombre: string
     porcentaje: number | string
     notas: Nota[]
+    tipo: Tipo
 }
 
 export interface Nota {
     id: number
     nombre: string
-    evaluacion: number | string
+    porcentaje: number | string
+    calificacion: number | string
 }
